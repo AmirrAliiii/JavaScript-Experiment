3d065f72613f6467b34ab8ba139f0a4e654d233d
AmirrAliiii <amiraliaghajaniagha@gmail.com>
2025-03-13 20:31:48 +0330

    guessing game

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const min = 1;
 const max = 100;
+const random = Math.floor(Math.random() * (max - min + 1) + min);
+let guess;
+let running = true;
+let attempts = 0;
 
-document.getElementById("MyButn").onclick = function () {
-  let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  document.getElementById("MyLabel").innerHTML = randomNumber;
-};
+while (running);
+{
+  guess = window.prompt(`Guess a number between ${min} and ${max}`);
+  guess = Number(guess);
+
+  if (isNaN(guess)) {
+    window.alert("Please enter a number");
+  } else if (guess < min || guess > max) {
+    window.alert("Please enter a number between 1 and 100");
+  } else if (guess > random) {
+    window.alert("Too high, try again");
+  } else if (guess < random) {
+    window.alert("Too low, try again");
+  } else if (guess == random) {
+    window.alert(
+      `You guessed it right congratulations! \n The number was ${random} \n Total attempts ${attempts}`
+    );
+    running = false;
+  }
+}
